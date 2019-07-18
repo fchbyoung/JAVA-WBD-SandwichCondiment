d350a50df094e59dc1ea36cbe2db64285a42f122
quan200699 <maplestory2012vn@gmail.com>
2019-07-18 09:21:37 +0700

    first commit

---
+++ src/main/java/controller/SandwichController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SandwichController {
+
+    @RequestMapping("/save")
+    public String save(@RequestParam(name = "condiment") String condiment, ModelMap model) {
+        model.addAttribute("condiment", condiment);
+        return "index";
+    }
+}
